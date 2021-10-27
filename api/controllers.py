2ba37f1f665b2a9af059fae7e4004017feb1c905
root <anbulldrewlock@gmail.com>
2021-10-27 16:00:25 -0500

    added most of dog/breed code, still errors

--- api/controllers.py
+++ api/controllers.py
@@ -25,7 +25,7 @@ from rest_framework import viewsets, filters, parsers, renderers
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics
 
 from django.contrib.auth import authenticate, login, logout
 from rest_framework.permissions import *
@@ -39,6 +39,8 @@ from api.pagination import *
 import json, datetime, pytz
 from django.core import serializers
 import requests
+
+from api.serializers import DogSerializer, BreedSerializer
 
 
 def home(request):
@@ -124,8 +126,164 @@ class Events(APIView):
     parser_classes = (parsers.JSONParser,parsers.FormParser)
     renderer_classes = (renderers.JSONRenderer, )
 
+    def get(self, request, format=None):
+        events = Event.objects.all()
+        json_data = serializers.serialize('json', events)
+        content = {'events': json_data}
+        return HttpResponse(json_data, content_type='json')
+
+    def post(self, request, *args, **kwargs):
+        print 'REQUEST DATA'
+        print str(request.data)
+
+        eventtype = request.data.get('eventtype')
+        timestamp = int(request.data.get('timestamp'))
+        userid = request.data.get('userid')
+        requestor = request.META['REMOTE_ADDR']
+
+        newEvent = Event(
+            eventtype=eventtype,
+            timestamp=datetime.datetime.fromtimestamp(timestamp/1000, pytz.utc),
+            userid=userid,
+            requestor=requestor
+        )
+
+        try:
+            newEvent.clean_fields()
+        except ValidationError as e:
+            print e
+            return Response({'success':False, 'error':e}, status=status.HTTP_400_BAD_REQUEST)
+
+        newEvent.save()
+        print 'New Event Logged from: ' + requestor
+        return Response({'success': True}, status=status.HTTP_200_OK)
 
 class ActivateIFTTT(APIView):
     permission_classes = (AllowAny,)
     parser_classes = (parsers.JSONParser,parsers.FormParser)
     renderer_classes = (renderers.JSONRenderer, )
+
+    def post(self,request):
+        print 'REQUEST DATA'
+        print str(request.data)
+
+        eventtype = request.data.get('eventtype')
+        timestamp = int(request.data.get('timestamp'))
+        requestor = request.META['REMOTE_ADDR']
+        api_key = ApiKey.objects.all().first()
+        event_hook = "test"
+
+        print "Creating New event"
+
+        newEvent = Event(
+            eventtype=eventtype,
+            timestamp=datetime.datetime.fromtimestamp(timestamp/1000, pytz.utc),
+            userid=str(api_key.owner),
+            requestor=requestor
+        )
+
+        print newEvent
+        print "Sending Device Event to IFTTT hook: " + str(event_hook)
+
+        #send the new event to IFTTT and print the result
+        event_req = requests.post('https://maker.ifttt.com/trigger/'+str(event_hook)+'/with/key/'+api_key.key, data= {
+            'value1' : timestamp,
+            'value2':  "\""+str(eventtype)+"\"",
+            'value3' : "\""+str(requestor)+"\""
+        })
+        print event_req.text
+
+        #check that the event is safe to store in the databse
+        try:
+            newEvent.clean_fields()
+        except ValidationError as e:
+            print e
+            return Response({'success':False, 'error':e}, status=status.HTTP_400_BAD_REQUEST)
+
+        #log the event in the DB
+        newEvent.save()
+        print 'New Event Logged'
+        return Response({'success': True}, status=status.HTTP_200_OK)
+
+class DogList(APIView):
+
+    def get(self, request, format=None):
+        dogs = Dog.objects.all()
+        serializer = DogSerializer(dogs, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = DogSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class DogDetail(APIView):
+    permission_classes = (AllowAny,)
+    parser_classes = (parsers.JSONParser,parsers.FormParser)
+    renderer_classes = (renderers.JSONRenderer, )
+
+    def get_object(self, pk):
+        try:
+            return Dog.objects.get(pk=pk)
+        except Dog.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        detail = self.get_object(pk)
+        serializer = DogSerializer(detail)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        detail = self.get_object(pk)
+        serializer = DogSerializer(detail, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        detail = self.get_object(pk)
+        detail.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class BreedList(APIView):
+
+    def get(self, request, format=None):
+        breeds = Breed.objects.all()
+        serializer = BreedSerializer(breeds, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = BreedSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class BreedDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Breed.objects.get(pk=pk)
+        except Breed.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        detail = self.get_object(pk)
+        serializer = BreedSerializer(detail)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        detail = self.get_object(pk)
+        serializer = BreedSerializer(detail, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        detail = self.get_object(pk)
+        detail.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- api/models.py
+++ api/models.py
@@ -29,3 +29,58 @@ class ApiKey(models.Model):
 
 class ApiKeyAdmin(admin.ModelAdmin):
     list_display = ('owner','key')
+
+class Breed(models.Model):
+    TINY = 'TN'
+    SMALL = 'SM'
+    MEDIUM = 'MD'
+    LARGE = 'LG'
+    SIZE_CHOICES = [
+        ('TN', 'Tiny'),
+        ('SM', 'Small'),
+        ('MD', 'Medium'),
+        ('LG', 'Large'),
+    ]
+    VALUE_CHOICES = [
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+        (5, 5),
+    ]
+    name = models.CharField(max_length=1000, blank=False)
+    size = models.CharField(
+        max_length = 2,
+        choices = SIZE_CHOICES,
+        default = MEDIUM,
+    )
+    friendliness = models.IntegerField(
+        choices = VALUE_CHOICES,
+        default = 3,
+    )
+    trainability = models.IntegerField(
+        choices = VALUE_CHOICES,
+        default = 3,
+    )
+    sheddingamount = models.IntegerField(
+        choices = VALUE_CHOICES,
+        default = 3,
+    )
+    exerciseneeds = models.IntegerField(
+        choices = VALUE_CHOICES,
+        default = 3,
+    )
+
+
+class Dog(models.Model):
+
+    name = models.CharField(max_length=1000, blank=False)
+    age = models.IntegerField(blank=False)
+    breed = models.ForeignKey(
+        Breed,
+        on_delete=models.CASCADE
+    )
+    gender = models.CharField(max_length=1000, blank=True)
+    color = models.CharField(max_length=1000, blank=True)
+    favoritefood = models.CharField(max_length=1000, blank=True)
+    favoritetoy = models.CharField(max_length=1000, blank=True)
