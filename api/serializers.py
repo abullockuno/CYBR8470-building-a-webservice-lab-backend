b26e37517144502d955c715ddd481a5244da698e
root <anbulldrewlock@gmail.com>
2021-10-27 15:57:24 -0500

    added most of dog/breed code, still errors

---
+++ api/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from api.models import Dog, Breed
+
+class DogSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Dog
+        fields = ['id', 'name', 'age', 'breed', 'gender', 'color','favoritefood', 'favoritetoy']
+
+class BreedSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Breed
+        fields = ['id','name','size','friendliness','trainability','sheddingamount','exerciseneeds']
